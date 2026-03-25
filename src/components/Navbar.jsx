e803000efb8f59213161bc24acc4c6a5c617461e
klconje4 <klconje@gmail.com>
2026-03-24 18:32:00 -0700

    Top Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="/Borrowed/blogo.png" alt="Borrowed logo" />
+        <span>Borrowed</span>
+      </div>
+      <div className="navbar-links">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/borrow">Borrow</NavLink>
+        <NavLink to="/sell">Sell</NavLink>
+        <NavLink to="/messages">Messages</NavLink>
+        <NavLink to="/profile">Profile</NavLink>
+      </div>
+    </nav>
+  )
+}
